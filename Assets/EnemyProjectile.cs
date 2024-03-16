4f0c92b9af405235030654e74bfc39b5990d505f
Burak Pişkin <burak.12000@hotmail.com>
2024-03-16 22:54:58 +0300

    Commit

--- Assets/EnemyProjectile.cs
+++ Assets/EnemyProjectile.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
+    public int damageAmount = 10;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Player Damage");
+            other.gameObject.GetComponent<HealthBar>().TakeDamage(damageAmount);
+            this.GetComponent<Rigidbody>().useGravity = true;
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            this.GetComponent<Rigidbody>().isKinematic = true;
+            this.GetComponent<BoxCollider>().isTrigger = true;
+            Destroy(this);
         }
     }
 }
